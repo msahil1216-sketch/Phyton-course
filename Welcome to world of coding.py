fca067aed747fdab1b0517ade4a6da0d1d7fc4c9
msahil1216-sketch <msahil1216@gmail.com>
2025-11-02 15:42:44 +0500

    Add files via upload

---
+++ Welcome to world of coding.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+#Print a Number
+print(4)
+#Next line after print
+print ("Hello Python \n")
+#Pass multiple arguments for rpinting
+print("hello",5)
+#End argument of Print Statement
+print ("Welcome to", end= "*")
