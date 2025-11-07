cf0ce15124ccc35d50df9f1a3c59778114d6e6e9
msahil1216-sketch <msahil1216@gmail.com>
2025-11-07 19:21:05 +0500

    Add files via upload

---
+++ Storing Birthdays.py
@@ -0,0 +1,12 @@
+#Storing Birthdays
+Rimuru_Tempest=("20th Feburary")
+Subaru_Natuski=("01st April")
+Alisa_Mikhailovna_Kujou=("07th November")
+Aquamarine_Hoshino=("28th December")
+Ruby_Hoshino=("28th December")
+
+print(Rimuru_Tempest)
+print(Subaru_Natuski)
+print(Alisa_Mikhailovna_Kujou)
+print(Aquamarine_Hoshino)
+print(Ruby_Hoshino)
