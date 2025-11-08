cf95d8318c07871217be6967c630b45659334a83
msahil1216-sketch <msahil1216@gmail.com>
2025-11-08 15:49:32 +0500

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,8 @@
+a = 5
+b = 2.5
+c = "Codingal"
+d = True
+print("Type of a: ", type(a))
+print("Type of b: ", type(b))
+print("Type of c: ", type(c))
+print("Type of d: ", type(d))
---
+++ Typecasting.py
@@ -0,0 +1,20 @@
+age = 15
+weight = 35.5
+name = "Sahil"
+is_student = True
+print("Name: ", name)
+print("Type of name: ", type(name))
+print("Age: ", age)
+print("Type of age: ", type(age))
+print("Weight: ", weight)
+print("Type of weight: ", type(weight))
+print("Is_Student: ", is_student )
+print("Type of is student: ", type(is_student))
+print("After Typecasting")
+
+age = str(age)
+weight = int(weight)
+print("Age: ", age)
+print("Type of age: ", type(age))
+print("Weight: ", weight)
+print("Type of weight: ", type(weight))
