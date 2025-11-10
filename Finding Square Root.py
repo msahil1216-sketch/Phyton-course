d64330d86af66f78f8657a2e493fbf9a86e933f2
msahil1216-sketch <msahil1216@gmail.com>
2025-11-10 17:35:40 +0500

    done

--- Finding Square Root.py
+++ Finding Square Root.py
@@ -1,4 +1,4 @@
 #Input Desired Number to find Squareroot
 Number = int(input("Please enter your number: "))
-Square_root = Number**2
+Square_root = Number**0.5
 print("Your number's Square root is ",Square_root)
