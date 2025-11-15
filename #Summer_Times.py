ed93e14172768dac22ccbaf9092d690231a0922d
msahil1216-sketch <msahil1216@gmail.com>
2025-11-15 19:13:27 +0500

    Add files via upload

---
+++ #Summer_Times.py
@@ -0,0 +1,9 @@
+# Which Temperature Which Clothes
+
+Temperature = int(input("Enter the temperature that surrounds you: "))
+
+if Temperature >= 25:
+    print("Perfect temperatures to wear any type of light clothing")
+
+else:
+    print("Too cold to wear light clothing, better to wear something warmer")
