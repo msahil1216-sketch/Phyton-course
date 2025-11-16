3da98b30f392813563e47e1fb93096dd7e63e5db
msahil1216-sketch <msahil1216@gmail.com>
2025-11-16 15:44:51 +0500

    Add files via upload

---
+++ NOT Operator.py
@@ -0,0 +1,26 @@
+#NOT Operator
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+
+if a != b:
+  print(a, 'and', b,'are not same')
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+  print("Hello")
+
+a = int(input("Enter a number"))
+
+if a%2 != 0:
+  print(a,"is not even")
+
+else:
+  print(a, "is even")
