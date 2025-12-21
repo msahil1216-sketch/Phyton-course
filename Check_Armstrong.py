474fda0e872a7185cd96bc6244c962ddb009ff86
msahil1216-sketch <msahil1216@gmail.com>
2025-12-21 15:40:13 +0500

    Add files via upload

---
+++ Check_Armstrong.py
@@ -0,0 +1,18 @@
+# Take input from the user
+num = int(input("Enter a Number: "))
+
+#initialise sume
+sum = 0
+
+#Find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+   # Display the result
+if num == sum:
+    print(num,"is an Armstrong number")
+else:
+    print(num,"is not an Armstrong number")
---
+++ Sum of Natural_Numbers.py
@@ -0,0 +1,10 @@
+#Input the value of terms
+n = int(input("Enter the value of terms: "))
+
+sum = 0 #intialise
+i = 1 #intialise
+while i <= n: #loop will run from 1 to n
+   sum = sum+i
+   i = i+1
+
+print("\nsum =", sum)
