ee7db05247a70af89d73455c855f7c2af88c3318
msahil1216-sketch <msahil1216@gmail.com>
2026-02-15 15:39:04 +0500

    Add files via upload

---
+++ Exit.02.py
@@ -0,0 +1,14 @@
+#Python program to demostarte
+#Exit()
+
+for i in range(10):
+
+    #If the value of i becomes
+    #5 then the program is forced
+    #To exit
+    if i == 5:
+
+        #Prints the exit message
+        print(exit)
+        exit()
+    print(i)   
