7debfe694d740bf6462940e87ec18a80660cc0d6
msahil1216-sketch <msahil1216@gmail.com>
2026-05-29 20:43:55 +0500

    Add files via upload

---
+++ Area and Perimeter of Circle.py
@@ -0,0 +1,16 @@
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius 
+
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+radius_value = float(input("Enter the radius of the circle: "))
+circle = Circle(radius_value)
+print(f"Area: {circle.area():.2f}")
+print(f"Perimeter: {circle.perimeter():.2f}")
