9e731536da17c43bf228f8f7ad12814694220871
msahil1216-sketch <msahil1216@gmail.com>
2026-07-04 15:43:16 +0500

    Add files via upload

--- lesson26/space.invader.game.py
+++ lesson26/space.invader.game.py
@@ -1,4 +1,5 @@
 import math
+import math
 import random
 import pygame
 
@@ -8,7 +9,7 @@ SCREEN_HEIGHT = 500
 PLAYER_START_X = 370
 PLAYER_START_Y = 380
 ENEMY_START_Y_MIN = 50
-ENEMY_START_X_MAX = 150
+ENEMY_START_Y_MAX = 150
 ENEMY_SPEED_X = 4
 ENEMY_SPEED_Y = 40
 BULLET_SPEED_Y = 10
@@ -21,7 +22,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Background
-background = pygame.image.load('background.png')
+background = pygame.image.load('background.jpg')
 
 # Caption and Icon
 pygame.display.set_caption("Space Invader")
@@ -29,7 +30,7 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
 # Player
-playerImg = pygame.image.load('player.png')
+playerImg = pygame.image.load('ufo.png')
 playerX = PLAYER_START_X
 playerY = PLAYER_START_Y
 playerX_change = 0
@@ -43,18 +44,19 @@ enemyY_change = []
 num_of_enemies = 6
 
 for _i in range(num_of_enemies):
-    enemyImg.append(pygame.imgae.load('enemy.png'))
-    enemyX.append(random.randit(0, SCREEN_WIDTH - 64)) #64 is the size of the enmy
-    enemyY.append(random.randit(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+    enemyImg.append(pygame.image.load('enemy.jpg'))
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64)) #64 is the size of the enmy
+    enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
     enemyX_change.append(ENEMY_SPEED_X)
     enemyX_change.append(ENEMY_SPEED_Y)
 
 # Bullet
-bulletImg = pygame.image.load('bullet.png')
+bulletImg = pygame.image.load('bullet.jpg')
 bulletX = 0 
 bulletY = PLAYER_START_Y
 bulletX_change = 0
-bulletY_change = BULLET_SPEED_Ybullet_start = 'ready'
+bulletY_change = BULLET_SPEED_Y
+bullet_state = "ready"
 
 # Score
 score_value = 0
@@ -97,7 +99,7 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
 # Game loop
 running = True
 while running:
-    screen.fill(0, 0, 0)
+    screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
@@ -116,7 +118,7 @@ while running:
 
     # Player Movement
     playerX += playerX_change
-    playerX = max(0, min(playerX, SCRENN_WIDTH - 64)) # 64 is the size of the player
+    playerX = max(0, min(playerX, SCREEN_WIDTH - 64)) # 64 is the size of the player
 
     # Enemy Movement
     for i in range(num_of_enemies):
